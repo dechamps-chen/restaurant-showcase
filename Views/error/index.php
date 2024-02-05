e6cdab36074797fce4fce76493a14b4035de0082
Dechamps Chen <dechamps.chen@gmail.com>
2024-02-05 11:12:30 +0100

    Ajout de la page 404

--- Views/error/index.php
+++ Views/error/index.php
@@ -3,4 +3,15 @@
 $title = "XBL - 后台界面";
 
 header("HTTP/1.0 404 Not Found");
-echo "<h1>404 Not Found</h1>";
+
+
+?>
+
+<div class="text-center">
+    <p class="text-base font-semibold text-indigo-600">404</p>
+    <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Oups</h1>
+    <p class="mt-6 text-base leading-7 text-gray-600">La page que vous recherchez semble introuvable.</p>
+    <div class="mt-10 flex items-center justify-center gap-x-6">
+        <a href="./home" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Retour à la page d'accueil</a>
+    </div>
+</div>
--- Views/errorbase.php
+++ Views/errorbase.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full">
 
 <head>
     <meta charset="UTF-8">
@@ -8,17 +8,14 @@
     <link href="http://localhost/applications/projet-xbl/public/output.css" type="text/css" rel="stylesheet">
 </head>
 
-<body>
-    <div class="container">
-        <main>
-            <?php echo $content ?>
-        </main>
+<body class="h-full relative">
+    <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8 relative">
+        <?php echo $content ?>
+    </main>
 
-        <footer class="text-center">
-            <p>© XBL 2024</p>
-        </footer>
-    </div>
-    <script src="http://localhost/applications/projet-xbl/public/script.js"></script>
+    <footer class="text-center bottom-0 left-0 right-0 absolute">
+        <p>© XBL 2024</p>
+    </footer>
 </body>
 
 </html>
