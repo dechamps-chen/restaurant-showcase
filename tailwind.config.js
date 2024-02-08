c6db32d51bccd8c3e32f69d467651c8d4a5e0cea
Dechamps Chen <dechamps.chen@gmail.com>
2024-02-08 16:50:17 +0100

    ajout Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,15 @@ module.exports = {
   content: [
     './index.php',
     './public/**/*.{html,js,php}',
-    './Views/**/*.{html,js,php}'
+    './Views/**/*.{html,js,php}',
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('flowbite/plugin')
   ],
 }
 
