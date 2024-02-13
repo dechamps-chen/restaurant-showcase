eb29f68db75894c31e3793d5c6dbc0cc7ee4678d
Dechamps Chen <dechamps.chen@gmail.com>
2024-02-13 14:34:40 +0100

    Modification nom du variable user

--- Models/UserModel.php
+++ Models/UserModel.php
@@ -8,7 +8,7 @@ use App\Entities\User;
 
 class UserModel extends Dbconnect
 {
-    public function isAuthentified(User $User)
+    public function isAuthentified(User $user)
     {
         try {
             $this->request = "SELECT id_user, name_user, password_user FROM user";
@@ -16,7 +16,7 @@ class UserModel extends Dbconnect
             $list = $result->fetchAll();
 
             foreach ($list as $value) {
-                if (($User->getName_user() == $value->name_user) && (password_verify($User->getPassword_user(), $value->password_user))) {
+                if (($user->getName_user() == $value->name_user) && (password_verify($user->getPassword_user(), $value->password_user))) {
                     return true;
                 }
             }
