213b6f98f9e1e03e3e34b0a52f6e49c9fdbf2a1b
Dechamps Chen <dechamps.chen@gmail.com>
2024-02-13 15:42:29 +0100

    Modification nom du fichier script.js en error.js

--- Views/loginbase.php
+++ Views/loginbase.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?></title>
     <link href="http://localhost/applications/projet-xbl/public/output.css" type="text/css" rel="stylesheet">
-    <script src="http://localhost/applications/projet-xbl/public/script.js"></script>
+    <script src="http://localhost/applications/projet-xbl/public/error.js"></script>
 </head>
 
 <body>
